e2eb66b905ea3c0a1e603c631ad26fc7ea185e55
Wolfgang Mattis <git@gedankenspieler.de>
2016-11-18 13:26:55 +0100

    Add files via upload
    Fixed: Now the settings for the original formatter are used.
    New: Allows to add a maximum length for ntext1line.

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ Helpful extensions for views
 This packet contains some helpful code for creating views for the framework YII2.
 
 YFormatter: Extended YII2-Formatter
-	New format: ntext1line => Returns the first line only (useful for text fields in grids)
+	New format: ntext1line => Returns the first line only or first part of the first line(useful for text fields in grids)
+	
+Examples:
+	In column definition:
+		'format' => 'ntext1line',		// Shows the first line only
+		'format' => ['ntext1line', 25]  // Shows a maximum of 25 characters of the fisrt line
 
 Installation
 ------------
--- YFormatter.php
+++ YFormatter.php
@@ -2,30 +2,51 @@
 
 namespace yasheena\view;
 
+use Yii;
 use yii\i18n\Formatter;
 use yii\helpers\Html;
 
 class YFormatter extends Formatter
 {
     /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+    	// Use the original fomatter settings also for this instance
+    	$config = Yii::$app->getComponents();
+    	$config = $config['formatter'];
+    	unset($config['class']);	// Ignore the class setting
+		Yii::configure($this, $config);
+       	parent::init();
+	}
+	
+	/**
      * Formats the value as an HTML-encoded plain text. If the value contains \n or \r
      * the first line will be returned only.
      * @param string $value the value to be formatted.
      * @return string the formatted result.
      */
-    public function asNtext1line($value)
+    public function asNtext1line($value, $maxlen = null)
     {
         if ($value === null) {
             return $this->nullDisplay;
         }
-        
+        $suffix = '';
         if ((false !== ($n = strpos($value, "\n"))) || (false !== ($n = strpos($value, "\r")))) {
         	if (($n > 0) && (substr($value, $n - 1, 1) == "\r")) {
         		$n--;
         	}
-        	$value = substr($value, 0, $n - 1) . ' ...';	 
+        	$value = substr($value, 0, $n - 1);
+        	$suffix = ' ...';	 
         }
-        return Html::encode($value);
+        if ($maxlen) {
+        	if ((strlen($value) + strlen($suffix) > $maxlen)) {
+				$suffix = ' ...';
+				$value = substr($value, 0, $maxlen - strlen($suffix));        		
+        	}
+        }
+        return Html::encode($value . $suffix);
     }
 
 }
